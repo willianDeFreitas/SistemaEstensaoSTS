fec1c361677ad0432380d3c140f494101ad604e1
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2019-09-04 20:24:51 -0300

    Versão inicial do projeto

---
+++ src/main/java/br/edu/iftm/extensao/SistemaExtensaoStsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.extensao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExtensaoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExtensaoStsApplication.class, args);
+	}
+
+}
